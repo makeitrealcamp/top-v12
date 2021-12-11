6ebef20afef4676883cae83f9d717a90bbe87d5c
Goye <karlos.goye@gmail.com>
2021-12-10 20:16:25 -0500

    Add CLI & State and props example

---
+++ javascript/react/cli/test-cli-app/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import Title from './Title';
+import './App.css';
+
+const style = {
+  backgroundColor: 'green'
+}
+
+const FIRST = 'Hola';
+
+function App() {
+  return (
+    <div style={style} className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Title firstPart={FIRST} />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
