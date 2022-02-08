1d61240a2a80bad98bf6842665efc5a9cdb9f133
Goye <karlos.goye@gmail.com>
2022-02-07 19:06:36 -0500

    Fix typo

--- typescript/backend-exercise/src/routes/user.ts
+++ typescript/backend-exercise/src/routes/user.ts
@@ -1,10 +1,9 @@
 import express from 'express'; 
-
-const userController = require("../controllers/user.controller");
+import { show, list, create } from '../controllers/user.controller';
 const router = express.Router();
 
-router.route("/:userId").get(userController.show);
-router.route("/").get(userController.list);
-router.route("/").post(userController.create);
+router.route("/:userId").get(show);
+router.route("/").get(list);
+router.route("/").post(create);
 
 export default router;
