6331ea160c08b56dbd1bdf614b73a5154285190e
Goye <karlos.goye@gmail.com>
2022-04-25 16:41:24 -0500

    Sequelize example

---
+++ node/express-sequelize/sequelize/index.js
@@ -0,0 +1,48 @@
+const { Sequelize } = require('sequelize');
+const { applyExtraSetup } = require('./extra-setup');
+
+// In a real app, you should keep the database connection URL as an environment variable.
+// But for this example, we will just use a local SQLite database.
+// const sequelize = new Sequelize(process.env.DB_CONNECTION_URL);
+/**
+ * Examples
+ * "postgres": {
+    "username": "postgres",
+    "password": "root",
+    "database": "groceries",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "mysql": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+ */
+const sequelize = new Sequelize({
+	dialect: 'sqlite',
+	storage: 'sqlite-example-database/example-db.sqlite',
+	logQueryParameters: true,
+	benchmark: true
+});
+
+const modelDefiners = [
+	require('./models/user.model'),
+	require('./models/instrument.model'),
+	require('./models/orchestra.model'),
+	// Add more models here...
+	// require('./models/item'),
+];
+
+// We define all models according to their files.
+for (const modelDefiner of modelDefiners) {
+	modelDefiner(sequelize);
+}
+
+// We execute any extra setup after the models are defined, such as adding associations.
+applyExtraSetup(sequelize);
+
+// We export the sequelize connection instance to be used around our app.
+module.exports = sequelize;
